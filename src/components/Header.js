8eb5e001a741b91c620a897504e1b04615c1bd7f
vakyla98 <vakyla98@ukr.net>
2020-07-21 11:43:01 +0300

    Create loop element dendering,add prettier, add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
+  const routes = [
+    { text: 'Home', path: '/', isExact: true },
+    { text: 'Cats', path: '/cats', isExact: false },
+    { text: 'Dogs', path: '/dogs', isExact: false },
+  ]
   const element = (
     <header>
       <nav>
         <h1>First react app</h1>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/cats">Cats</Link>
-          </li>
-          <li>
-            <Link to="/dogs">Dogs</Link>
-          </li>
+          {routes.map((route, i) => (
+            <li key={i}>
+              <NavLink to={route.path} exact={route.isExact} activeClassName="active-link">
+                {route.text}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
-  );
-  return element;
+  )
+  return element
 }
 
-export default Header;
+export default Header
