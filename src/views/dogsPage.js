7aeabc84ec28e7b1b7618e437382169568f99126
vakyla98 <vakyla98@ukr.net>
2020-07-25 20:47:09 +0300

    Add hooks to dogsPage

--- src/views/dogsPage.js
+++ src/views/dogsPage.js
@@ -1,54 +1,46 @@
-import React from 'react'
-import { dogService } from '../services'
+import React, { useState, useEffect, } from "react";
 import { connect } from 'react-redux'
+
+import { dogService } from '../services'
 
 import { addImage } from '../redux/actions'
 
 import Button from '@material-ui/core/Button'
 
-class DogsPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            imgUrl: '',
-        }
-        this.getImage = this.getImage.bind(this)
-        this.testDispatch = this.testDispatch.bind(this)
-    }
-    async componentDidMount() {
-        this.getImage()
-    }
-    async getImage() {
-        this.setState({
-            imgUrl: await dogService.getURL(),
-        })
-    }
-    testDispatch() {
+function DogsPage({ addImage }) {
+    const [imgUrl, setImgUrl] = useState('');
 
+    useEffect(() => { setNewImage() }, []);
+
+    async function setNewImage() {
+        let url = await dogService.getURL()
+        setImgUrl(url)
     }
-    render() {
-        let image
-        if (this.state.imgUrl) {
-            image = <img src={this.state.imgUrl} alt="Cat" />
-        } else {
-            image = <span>Loading...</span>
-        }
-        return (
-            <div>
-                <h1>This is a cat!</h1>
-                <Button onClick={this.getImage} variant="contained" color="primary">
-                    Get new
-                </Button>
-                <Button onClick={() => this.props.addImage(this.state.imgUrl)}
-                    variant="contained"
-                    color="primary">
-                    Add image
-                </Button>
-                <hr></hr>
-                {image}
-            </div>
-        )
+
+
+    let image
+    if (imgUrl) {
+        image = <img src={imgUrl} alt="Dog" />
+    } else {
+        image = <span>Loading...</span>
     }
+
+    return (
+        <div>
+            <h1>This is a cat!</h1>
+            <Button onClick={setNewImage} variant="contained" color="primary">
+                Get new
+            </Button>
+            <Button onClick={() => addImage(imgUrl)}
+                variant="contained"
+                color="primary">
+                Add image
+            </Button>
+            <hr></hr>
+            {image}
+        </div>
+    )
+
 }
 
 const mapDispatchToProps = {
