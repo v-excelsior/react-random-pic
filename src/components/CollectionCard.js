989595e19bae208087a66bfdad221453f33dc682
vakyla98 <vakyla98@ukr.net>
2020-07-28 21:57:02 +0300

    Finish deleting image

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { deleteImage } from '../redux/actions'
 
-
-function CollectionCard(props, { deleteImage }) {
-
-
+function CollectionCard({ imageUrl, deleteImage }) {
     return (
         <div className="collection-card">
-            <button className='collection-card__delete-btn' onClick={deleteImage(props.imageUrl)}></button>
-            <img src={props.imageUrl} className="collection-card__image" alt="Collection card" />
+            <button
+                className="collection-card__delete-btn"
+                onClick={() => deleteImage(imageUrl)}
+            ></button>
+            <img src={imageUrl} className="collection-card__image" alt="Collection card" />
         </div>
     )
 }
 
 const mapDispatchToProps = {
-    deleteImage
+    deleteImage,
 }
 
 export default connect(null, mapDispatchToProps)(CollectionCard)
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,18 +1,16 @@
-import { ADD_IMAGE,DELETE_IMAGE } from './types'
-
-export function addImage(imageUrl, animalType) {
-    return {
-        type: ADD_IMAGE,
-        url: imageUrl,
-        animal: animalType,
-    }
-}
-
+import { ADD_IMAGE, DELETE_IMAGE } from './types'
 
 export function deleteImage(imageUrl) {
     return {
         type: DELETE_IMAGE,
         url: imageUrl,
         // animal: animalType,
+    }
+}
+export function addImage(imageUrl, animalType) {
+    return {
+        type: ADD_IMAGE,
+        url: imageUrl,
+        animal: animalType,
     }
 }
