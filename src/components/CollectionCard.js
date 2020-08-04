7f9467cbbda701b517d4c2b7b69c1be3c4f81a90
vakyla98 <vakyla98@ukr.net>
2020-08-04 23:16:57 +0300

    Rewrite buttom with MuiIcons

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { deleteImage } from '../redux/actions'
 
+import CloseIcon from '@material-ui/icons/Close'
+
 function CollectionCard({ imageUrl, deleteImage }) {
     return (
         <div className="collection-card">
-            <button className="collection-card__delete-btn" onClick={() => deleteImage(imageUrl)} />
+            <button className="collection-card__delete-btn" onClick={() => deleteImage(imageUrl)}>
+                <CloseIcon color="secondary"/>
+            </button>
             <img src={imageUrl} className="collection-card__image" alt="Collection card" />
         </div>
     )
